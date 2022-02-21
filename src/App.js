f501f5556d2c7b1ad9729d0ef5ae36e915867949
Djibril <djibrilsamassa.pro@gmail.com>
2022-02-21 14:38:04 +0100

    Login commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import Home from "./components/Home"
+import Login from "./components/Login"
 import { BrowserRouter, Switch, Link, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
+    <nav>
+      <Link to={"/auth"}>Se connecter | </Link>
+      <Link to={"/"}>Homepage</Link>
+    </nav>
     <Switch>
+      <Route exact path="/auth" component={Login}/>
       <Route exact path ="/" component={Home}/>
     </Switch>
     
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,22 @@
+import { useForm } from "react-hook-form"
 
 
 export default function Login(){
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = data =>{
+        console.log(data)  
+    }
+
+
     return(
         <div>
             <h2>Login</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" required maxLength={15} {...register("username")}/>
+                <button type="submit">envoyer</button>
+            </form>
         </div>
-    )
+    )  
 }
